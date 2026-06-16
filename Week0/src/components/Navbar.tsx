ef4e15b08ccf0ceaf97f8a81ab7b116ab5e5ddab
nataliabarredat-pixel <nataliabarredat@gmail.com>
2026-06-16 11:03:47 -0600

    Update Navbar.tsx

--- Week0/src/components/Navbar.tsx
+++ Week0/src/components/Navbar.tsx
@@ -88,7 +88,7 @@ export default function Navbar() {
                   : "text-stone-600 hover:text-rose-600 dark:text-stone-300 dark:hover:text-rose-400"
               }`}
             >
-              <BarChart2 className="h-4 w-4" />
+              <Compass className="h-4 w-4" />
               <span>Product</span>
             </Link>
 
@@ -100,7 +100,7 @@ export default function Navbar() {
                   : "text-stone-600 hover:text-rose-600 dark:text-stone-300 dark:hover:text-rose-400"
               }`}
             >
-              <BarChart2 className="h-4 w-4" />
+              <DollarSign className="h-4 w-4" />
               <span>Pricing</span>
             </Link>
             
